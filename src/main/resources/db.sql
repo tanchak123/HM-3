67e762e2e7c09ed63d77c08d7e70a4565df72b0c
Andrey Tanchak <tanchak12@gmail.com>
2020-10-12 17:05:19 +0300

    hm-4 resolve

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,4 +1,10 @@
-CREATE TABLE IF NOT EXISTS text_processorss (
+CREATE PROCEDURE Save_On_Db(texter varchar(255))
+    LANGUAGE SQL
+AS $$
+INSERT INTO text_processor (text) values (texter);
+$$;
+create domain clob as text;
+CREATE TABLE IF NOT EXISTS text_processor (
                                               text_id serial PRIMARY KEY,
-                                              name clob
+                                              text clob
 );
